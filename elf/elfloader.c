8ceb2eb5ef7f50d482bf84d2c6badbeee2c27d81
desched <desched@cf167-08.cs.wwu.edu>
2025-11-18 21:20:41 -0800

    #2

--- elf/elfloader.c
+++ elf/elfloader.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]) {
   // e_ident id = head.e_ident;
   if(head.e_ident[0] == 0x7f && head.e_ident[1] == 'E'
      && head.e_ident[2] == 'L' && head.e_ident[3] == 'F') {
-    printf("Confirmed Elf File\n");
+    //printf("Confirmed Elf File\n");
   }
   
   // int offset = 0;
@@ -34,18 +34,18 @@ int main(int argc, char *argv[]) {
   Elf64_Phdr phd;
   fread(&phd, sizeof(phd), 1, file);
   if(phd.p_type == PT_LOAD){  // Checks if the type is LOAD
-    printf("type = LOAD\n");
+    //printf("type = LOAD\n");
   } else {
     fclose(file);
     return 3;
   }
   if(phd.p_flags & PF_X){  // Checks if the file is executable
-    printf("executable!\n");
+    //printf("executable!\n");
   } else {
     fclose(file);
     return 3;
   }
-  printf("phd.p_type == %u, phd.p_offset = %u, phd.p_vaddr = %u\n", phd.p_type,  phd.p_offset, phd.p_vaddr);
+  //printf("phd.p_type == %u, phd.p_offset = %u, phd.p_vaddr = %u\n", phd.p_type,  phd.p_offset, phd.p_vaddr);
   fseek(file, phd.p_offset, SEEK_SET);
   void* codep = mmap(NULL, phd.p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC,
 		     MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
